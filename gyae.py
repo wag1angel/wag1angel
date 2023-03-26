f494ea4433c4f2d119c5a08575f42461f53be3d1
wag1angel <121037473+wag1angel@users.noreply.github.com>
2023-03-26 16:16:42 +0200

    Update gyae.py

--- gyae.py
+++ gyae.py
@@ -63,23 +63,23 @@ def introduction():
     FRM_anvandar_INFO.pack(fill=tk.BOTH, expand=True)
 
     # Add title
-    LBL_title = tk.Label(master=FRM_anvandar_INFO, text="Aim Trainer", font=("Arial", 30, "bold"), pady=20, bg='black', fg='magenta2')
+    LBL_title = tk.Label(master=FRM_anvandar_INFO, text="Aim Trainer", font=("open sans", 30, "bold"), pady=20, bg='black', fg='magenta2')
     LBL_title.pack()
 
     # Centered text
     FRM_center = tk.Frame(master=FRM_anvandar_INFO, bg='black')
     FRM_center.pack(fill=tk.BOTH, expand=True)
 
-    LBL_anvandare = tk.Label(master=FRM_center, text="Välj användarnamn: ", font=("Arial", 20), pady=10, bg='black', fg='magenta2')
+    LBL_anvandare = tk.Label(master=FRM_center, text="Välj användarnamn: ", font=("open sans", 20), pady=10, bg='black', fg='magenta2')
     LBL_anvandare.pack()
 
-    ENT_anvandarnamn = tk.Entry(master=FRM_center, font=("Arial", 12), bg='black', fg='magenta2')
+    ENT_anvandarnamn = tk.Entry(master=FRM_center, font=("open sans", 12), bg='black', fg='magenta2')
     ENT_anvandarnamn.pack(pady=5)
 
-    BTN_SUBMIT = tk.Button(master=FRM_center, text="skicka in", command=spel_nivaer, font=("Arial", 12), bg='magenta2', fg='black')
+    BTN_SUBMIT = tk.Button(master=FRM_center, text="skicka in", command=spel_nivaer, font=("open sans", 12), bg='magenta2', fg='black')
     BTN_SUBMIT.pack(pady=10)
 
-    LBL_spel_DETAILS = tk.Label(master=FRM_center, text="meningen med spelet \n är att klicka på svarta boxen \n så snabbt som möjligt \n innan tiden tar slut.", font=("Arial", 13), bg='black', fg='magenta2')
+    LBL_spel_DETAILS = tk.Label(master=FRM_center, text="meningen med spelet \n är att klicka på svarta boxen \n så snabbt som möjligt \n innan tiden tar slut.", font=("open sans", 13), bg='black', fg='magenta2')
     LBL_spel_DETAILS.pack(pady=50)
 
 
@@ -109,14 +109,14 @@ def spel_nivaer():
     FRM_nivaer = tk.Frame(master=window, height=650, width=550, borderwidth=4, relief=tk.SUNKEN, background='black')
     FRM_nivaer.pack(fill=tk.BOTH)
 
-    LBL_niva = tk.Label(master=FRM_nivaer, text="välj nivå:", pady=5, font=("Arial", 15), background='black', foreground='magenta2')
+    LBL_niva = tk.Label(master=FRM_nivaer, text="välj nivå:", pady=5, font=("open sans", 15), background='black', foreground='magenta2')
     LBL_niva.pack()
 
     BTN_nyborjare = tk.Button(
         master=FRM_nivaer, 
         text="nybörjare", 
         height=3, width=15, 
-        font=("Arial", 12),
+        font=("open sans", 12),
         command=lambda : spel_layout(spel_installningar['användarnamn'], spel_installningar['nybörjare']),
         background='black', foreground='magenta2'
         )
@@ -125,7 +125,7 @@ def spel_nivaer():
         master=FRM_nivaer, 
         text="normal", 
         height=3, width=15,
-        font=("Arial", 12),
+        font=("open sans", 12),
         command=lambda : spel_layout(spel_installningar['användarnamn'], spel_installningar['normal']),
         background='black', foreground='magenta2'
         )
@@ -134,7 +134,7 @@ def spel_nivaer():
         master=FRM_nivaer, 
         text="svår", 
         height=3, width=15,
-        font=("Arial", 12),
+        font=("open sans", 12),
         command=lambda : spel_layout(spel_installningar['användarnamn'], spel_installningar['svår']),
         background='black', foreground='magenta2'
         )
@@ -181,9 +181,9 @@ def spel_layout(anvandarnamn: str, difficulty: List):
 
     
     FRM_INFO = tk.Frame(master=window, padx=10, pady=10, bg='black')
-    LBL_anvandarnamn = tk.Label(master=FRM_INFO, text=anvandarnamn, font=("Arial", 15), fg='magenta2', bg='black')
-    LBL_POINTS = tk.Label(master=FRM_INFO, text=0, font=("Arial", 15), fg='magenta2', bg='black')
-    LBL_tid = tk.Label(master=FRM_INFO, text=10, font=("Arial", 15), fg='magenta2', bg='black')
+    LBL_anvandarnamn = tk.Label(master=FRM_INFO, text=anvandarnamn, font=("open sans", 15), fg='magenta2', bg='black')
+    LBL_POINTS = tk.Label(master=FRM_INFO, text=0, font=("open sans", 15), fg='magenta2', bg='black')
+    LBL_tid = tk.Label(master=FRM_INFO, text=10, font=("open sans", 15), fg='magenta2', bg='black')
 
     FRM_INFO.pack(fill=tk.BOTH, side=tk.TOP)
     LBL_anvandarnamn.pack(side=tk.LEFT)
@@ -233,7 +233,7 @@ def timer():
     if LBL_tid['text'] == 0:
         BTN_maltavla.place_forget()
 
-        LBL_poang = tk.Label(master=FRM_spel, text=f"Poäng  : {LBL_POINTS['text']}", font=("Arial", 20), bg="black", fg="#EE82EE")
+        LBL_poang = tk.Label(master=FRM_spel, text=f"Poäng  : {LBL_POINTS['text']}", font=("open sans", 20), bg="black", fg="#EE82EE")
         LBL_poang.place(x=150, y=170)
 
         BTN_aterstall = tk.Button(master=FRM_spel, text="Försök Igen", command=aterstall, width=10, bg="black", fg="#EE82EE")
